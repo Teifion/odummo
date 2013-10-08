0ddd3da8824f1ef3a2b17f9cf8beb922366e6bd5
Teifion <sarkalian@gmail.com>
2013-10-09 00:26:27 +0100

    Stub functions

--- views/game.py
+++ views/game.py
@@ -1,3 +1,17 @@
 """
 Views relating to games of Othello.
 """
+
+from ..config import config
+
+def new_game(request):
+    pass
+
+def rematch(request):
+    pass
+
+def view_game(request):
+    pass
+
+def make_move(request):
+    pass
