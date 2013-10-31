0f30310ab8039b6b87a75d94b247126ec3985039
Teifion <sarkalian@gmail.com>
2013-10-31 00:48:59 +0000

    Used tests to test some stuff, no formal unit testing yet though

--- tests/rule_tests.py
+++ tests/rule_tests.py
@@ -1,4 +1,9 @@
 import unittest
+from collections import namedtuple
+
+BoardObject = namedtuple('BoardObject', ['current_state', 'turn'])
+
+b0 = '1111111 22221111221111112222222122121221222112112222111122221111'
 
 b1 = """
         
@@ -14,5 +19,9 @@ from ..lib import rules
 
 class OdummoRuleTester(unittest.TestCase):
     def test_validity(self):
-        print(rules.is_move_valid(b1, 1+1, 62))
-        print(rules.is_move_valid(b1, 2+1, 62))
+        # print(rules.is_move_valid(b1, 1+1, 62))
+        # print(rules.is_move_valid(b1, 2+1, 62))
+        
+        # print(rules.check_for_win(BoardObject(b0, 1)))
+        # print(rules.check_for_win(BoardObject(b0, 2)))
+        pass
